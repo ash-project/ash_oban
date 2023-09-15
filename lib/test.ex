8775a1c52bcdd851199062ff8a8ff2899859821d
Zach Daniel <zach@zachdaniel.dev>
2023-09-15 15:32:47 -0400

    improvement: support apis/resources for ash_oban

--- lib/test.ex
+++ lib/test.ex
@@ -1,26 +1,53 @@
 defmodule AshOban.Test do
   @moduledoc "Helpers for testing ash_oban triggers"
 
-  def schedule_and_run_triggers(resource) do
-    triggers =
-      AshOban.Info.oban_triggers(resource)
+  @mix_app Mix.Project.config()[:app]
 
-    Enum.each(triggers, fn trigger ->
-      AshOban.schedule(resource, trigger)
-    end)
-
-    queues =
-      triggers
-      |> Enum.map(& &1.queue)
-      |> Enum.uniq()
-
-    # we drain each queue twice to do schedulers and then workers
-    Enum.reduce(queues ++ queues, %{}, fn queue, acc ->
-      [queue: queue]
-      |> Oban.drain_queue()
+  def schedule_and_run_triggers() do
+    @mix_app
+    |> Application.get_env(:ash_apis, [])
+    |> List.wrap()
+    |> Enum.reduce(%{}, fn api, acc ->
+      api
+      |> schedule_and_run_triggers()
       |> Map.merge(acc, fn _key, left, right ->
         left + right
       end)
     end)
+  end
+
+  def schedule_and_run_triggers(resource_or_api) do
+    if Spark.Dsl.is?(resource_or_api, Ash.Api) do
+      resource_or_api
+      |> Ash.Api.Info.resources()
+      |> Enum.reduce(%{}, fn resource, acc ->
+        resource
+        |> schedule_and_run_triggers()
+        |> Map.merge(acc, fn _key, left, right ->
+          left + right
+        end)
+      end)
+    else
+      triggers =
+        AshOban.Info.oban_triggers(resource_or_api)
+
+      Enum.each(triggers, fn trigger ->
+        AshOban.schedule(resource_or_api, trigger)
+      end)
+
+      queues =
+        triggers
+        |> Enum.map(& &1.queue)
+        |> Enum.uniq()
+
+      # we drain each queue twice to do schedulers and then workers
+      Enum.reduce(queues ++ queues, %{}, fn queue, acc ->
+        [queue: queue]
+        |> Oban.drain_queue()
+        |> Map.merge(acc, fn _key, left, right ->
+          left + right
+        end)
+      end)
+    end
   end
 end
