c1351f51f94305a0d7aaf3ab7daccea9d8f1ba6e
Zach Daniel <zach@zachdaniel.dev>
2025-09-21 08:50:29 -0400

    test: fix igniter tests

--- test/mix/tasks/ash_oban/install_test.exs
+++ test/mix/tasks/ash_oban/install_test.exs
@@ -27,33 +27,34 @@ defmodule Mix.Tasks.AshOban.InstallTest do
   test "adds ash_oban to the formatter", %{igniter: igniter} do
     igniter
     |> assert_has_patch(".formatter.exs", """
-       ...|
-    2 2   |[
-    3 3   |  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
-    4   - |  import_deps: [:oban]
-      4 + |  import_deps: [:ash_oban, :oban]
-    5 5   |]
-    6 6   |
+    ...|
+       |[
+       |  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+     - |  import_deps: [:oban]
+     + |  import_deps: [:ash_oban, :oban]
+       |]
+       |
     """)
   end
 
   test "adds the cron plugin to the oban config", %{igniter: igniter} do
     igniter
     |> assert_has_patch("config/config.exs", ~S'''
-    1  1   |import Config
-    2  2   |
-       3 + |config :ash_oban, pro?: false
-       4 + |
-    3  5   |config :test, Oban,
-    4  6   |  engine: Oban.Engines.Basic,
-    5  7   |  notifier: Oban.Notifiers.Postgres,
-    6  8   |  queues: [default: 10],
-    7    - |  repo: Test.Repo
-       9 + |  repo: Test.Repo,
-      10 + |  plugins: [{Oban.Plugins.Cron, []}]
-    8 11   |
-    9 12   |import_config "#{config_env()}.exs"
-        ...|
+      |import Config
+      |
+    + |config :ash_oban, pro?: false
+    ''')
+    |> assert_has_patch("config/config.exs", ~S'''
+       |config :test, Oban,
+       |  engine: Oban.Engines.Basic,
+       |  notifier: Oban.Notifiers.Postgres,
+       |  queues: [default: 10],
+     - |  repo: Test.Repo
+     + |  repo: Test.Repo,
+     + |  plugins: [{Oban.Plugins.Cron, []}]
+       |
+       |import_config "#{config_env()}.exs"
+    ...|
     ''')
   end
 
@@ -62,20 +63,20 @@ defmodule Mix.Tasks.AshOban.InstallTest do
   } do
     igniter
     |> assert_has_patch("lib/test/application.ex", """
-         ...|
-     6  6   |  @impl true
-     7  7   |  def start(_type, _args) do
-     8    - |    children = [{Oban, Application.fetch_env!(:test, Oban)}]
-        8 + |    children = [
-        9 + |      {Oban,
-       10 + |       AshOban.config(
-       11 + |         Application.fetch_env!(:test, :ash_domains),
-       12 + |         Application.fetch_env!(:test, Oban)
-       13 + |       )}
-       14 + |    ]
-     9 15   |
-    10 16   |    opts = [strategy: :one_for_one, name: Test.Supervisor]
-         ...|
+    ...|
+       |  @impl true
+       |  def start(_type, _args) do
+     - |    children = [{Oban, Application.fetch_env!(:test, Oban)}]
+     + |    children = [
+     + |      {Oban,
+     + |       AshOban.config(
+     + |         Application.fetch_env!(:test, :ash_domains),
+     + |         Application.fetch_env!(:test, Oban)
+     + |       )}
+     + |    ]
+       |
+       |    opts = [strategy: :one_for_one, name: Test.Supervisor]
+    ...|
     """)
   end
 end
